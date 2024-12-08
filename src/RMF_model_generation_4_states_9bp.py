e976cd885977cde8c8d28571282a20179ff90767
yangli-evo <yangli.evo@gmail.com>
2024-12-08 14:50:35 -0500

    Add files via upload

---
+++ src/RMF_model_generation_4_states_9bp.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import numpy.random as nrand
+import matplotlib.pyplot as plt
+import pandas as pd
+import itertools
+import copy
+import pickle
+import multiprocessing
+
+
+# In[4]:
+
+
+# RMF landscape parameters -----------------------------------------
+# Change parameters to get fitness landscape of different variable site.
+# F(gt) = -cD(wt,gt)+N(std)
+
+N = 10  # number of variable site
+c = 1
+
+# Initialize genotype 0-1 space
+gt_lst = np.array(list(map(list, itertools.product([0, 1, 2, 3], repeat=N))))
+
+print(gt_lst)
+print(len(gt_lst))
+# In[5]:
+
+
+# '''
+# Create Polynomial fitness landscape
+RMF_landscape_list={}
+RMF_landscape_list[1]=[]
+RMF_landscape_list[2]=[]
+# for idx in range(1,101,2):
+#     print(idx,'    ',end='\r')
+for std in [10/n for n in range(1,21)]: # std is sampled from 0.5 to 10
+    print(std)
+    wt = nrand.randint(4, size=N) # Set wildtype genotype
+
+    # Calculate fitness and combine that to FL
+    fitness_lst = -c*np.sum(wt != gt_lst,axis=1)+nrand.normal(scale=std,size = np.power(4, N))
+
+    # Normalize fitness
+    MIN = np.min(fitness_lst)
+    MAX = np.max(fitness_lst)
+    fitness_lst = (fitness_lst - MIN) / (MAX - MIN)
+
+    # Combine fitness to genotype 0-1 space
+    fitness_landscape = np.concatenate((gt_lst,fitness_lst.reshape([-1,1])),axis=1)
+
+    # if there are 10 landscapes in the current RMF_landscape_list[idx],
+    # go to the next idx
+    print(fitness_landscape)
+    output=open('./RMF10bp/RMF_10bp_std'+str(round(std, 2))+'.txt', 'w')
+    hand_list=['seq_info', 'fitness']
+    output.write('\t'.join(hand_list)+'\n')
+    mapping = {0: 'A', 1: 'T', 2: 'C', 3: 'G'}
+
+    for genetic_info in fitness_landscape:
+        bases = [mapping[number] for number in genetic_info[0:-1]]
+        seq_info=''.join(bases)
+        fitness=str(genetic_info[-1])
+        output_list=[seq_info, fitness]
+        output.write('\t'.join(output_list)+'\n')
+    output.close()
+
+
+
+
+'''
+    if len(RMF_landscape_list[1]) <10:
+        RMF_landscape_list[1].append(fitness_landscape)
+    else:
+        RMF_landscape_list[1+1].append(fitness_landscape)
+
+
+# In[7]:
+
+# print(RMF_landscape_list)
+# print(RMF_landscape_list[1])
+print(RMF_landscape_list[2])
+
+with open(f'RMF_{N}_landscape_list_100X10.pkl','wb') as f:
+    pickle.dump(RMF_landscape_list,f)
+'''
